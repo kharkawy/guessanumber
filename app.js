1109e49a850d9f8f66c4f9b36b4e63b1fc3a4320
kharkawy <kar.harkawy@gmail.com>
2019-09-22 21:21:01 +0200

    added input validation for numbers between 0 and 100

--- app.js
+++ app.js
@@ -6,15 +6,15 @@
 //   - if higher than generated number, display that it's higher +
 //   - if the same, display that you guessed +
 //4. Update amount of tries +
-//5. Add the last guess to guesses panel
+//5. Add the last guess to guesses panel +
 //6. If you guessed, disable guess button +
 //7. Make a new game function(reset input!)
+//8. Add input validation +
 
 var randomNumber;
 var playerGuess;
-var isGamePlaying;
 var amountOfTries;
-var previousGuesses;
+var isInputValid;
 
 var guessInput = document.getElementById("guess-input");
 var guessButton = document.getElementById("guess-btn");
@@ -22,27 +22,30 @@ var guessResult = document.getElementById("guess-result");
 var newGameButton = document.getElementById("new-game-btn");
 var triesSpan = document.getElementById("tries");
 var previousGuessesDiv = document.getElementById("previous-guesses");
+var inputLabel = document.getElementById("guess-label");
 
 guessButton.addEventListener("click", function() {
-  if (isGamePlaying) {
-    playerGuess = guessInput.value;
+  playerGuess = guessInput.value;
+  validateInput(playerGuess);
+  //if validInput is true
+
+  if (isInputValid) {
     compareNumbers();
-
-    var newPreviousGuess = document.createElement("span");
-    newPreviousGuess.innerHTML = playerGuess;
-    previousGuessesDiv.appendChild(newPreviousGuess);
-
-    amountOfTries++;
-    triesSpan.innerHTML = amountOfTries;
+    updatePreviousGuesses(playerGuess);
+    updateTries();
   }
 });
 
+newGameButton.addEventListener("click", function() {
+  startGame();
+});
+
 function startGame() {
-  isGamePlaying = true;
   generateNumber();
   amountOfTries = 0;
   triesSpan.innerHTML = amountOfTries;
-  previousGuesses = [];
+  previousGuessesDiv.innerHTML = "";
+  guessResult.innerHTML = "";
 }
 
 function generateNumber() {
@@ -60,10 +63,40 @@ function compareNumbers() {
     guessResult.innerHTML = player + " is too low!";
   } else if (player == computer) {
     guessResult.innerHTML = "It's " + player + "! You guessed!";
-    isGamePlaying = false;
-    guessButton.classList.add("inactive");
-    newGameButton.classList.remove("inactive");
+    gameOver();
   }
 }
 
+function updatePreviousGuesses(guess) {
+  var newPreviousGuess = document.createElement("span");
+  newPreviousGuess.innerHTML = guess;
+  previousGuessesDiv.appendChild(newPreviousGuess);
+}
+
+function updateTries() {
+  amountOfTries++;
+  triesSpan.innerHTML = amountOfTries;
+}
+
+function gameOver() {
+  guessButton.classList.add("inactive");
+  newGameButton.classList.remove("inactive");
+}
+
+function validateInput(inputValue) {
+  if (inputValue.match(/^[A-Za-z]+$/)) {
+    inputLabel.innerHTML = "Please input only numbers between 0 and 100";
+    isInputValid = false;
+    console.log(1);
+  } else if (inputValue < 0 || inputValue > 100) {
+    inputLabel.innerHTML = "Please input only numbers between 0 and 100";
+    isInputValid = false;
+    console.log(2);
+  } else {
+    inputLabel.innerHTML = "";
+    isInputValid = true;
+  }
+
+  console.log(isInputValid);
+}
 startGame();
